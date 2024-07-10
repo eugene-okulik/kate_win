d91b4aae6c1217d614556187ace5660212f5a46b
Kate <katerina.ev@gmail.com>
2024-07-10 17:32:03 +0400

    homework 3 done

---
+++ homework/kate_evtihevich/homework_3/task_3.py
@@ -0,0 +1,8 @@
+import math
+
+number_1 = 50
+number_2 = 100
+average = (number_1 + number_2) / 2
+geometric_average = math.sqrt(number_1 * number_2)
+print('average is', average)
+print('geometric average is', geometric_average)
---
+++ homework/kate_evtihevich/homework_3/task_4.py
@@ -0,0 +1,9 @@
+import math
+
+leg_1 = 3
+leg_2 = 4
+hypotenuse = math.sqrt(leg_1**2 + leg_2**2)
+area = (leg_1 * leg_2) / 2
+
+print('hypotenuse = ', hypotenuse)
+print('area = ', area)
