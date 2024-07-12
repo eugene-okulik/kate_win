eca4c0a7dbdeee1f2398b10eb243e92c1d2e8193
Kate <katerina.ev@gmail.com>
2024-07-12 17:47:41 +0400

    homework 4 done

---
+++ homework/kate_evtihevich/homework_4/task.py
@@ -0,0 +1,25 @@
+my_dict = {
+    'tuple': (1, 7, 8, 11, 12),
+    'list': ['abc', '123', 'sun', 'moon', 1488],
+    'set': {'Artem', 'Marsi', 78, 89, True, False},
+    'dict': {'type1': 'Number',
+             'type2': 'String',
+             'type3': 'Boolean',
+             'type4': 'Float',
+             'type5': 'List',
+             'type6': 'Dictionary',
+             'type7': 'Set',
+             'type8': 'Tuple'}
+}
+
+print(my_dict['tuple'][-1])
+
+my_dict['list'].append('jupiter')
+my_dict['list'].pop(1)
+
+my_dict['set'].add('Victor')
+my_dict['set'].remove('Artem')
+
+my_dict['dict']['i am a tuple'] = 'Tuple'
+my_dict['dict'].pop('type8')
+print(my_dict)
