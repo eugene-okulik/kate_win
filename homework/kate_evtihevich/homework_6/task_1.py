144fb8aec9e673f6d8a32f75d1bfb1af5c3bf095
Kate <katerina.ev@gmail.com>
2024-07-18 13:25:15 +0400

    homework 6 updated

--- homework/kate_evtihevich/homework_6/task_1.py
+++ homework/kate_evtihevich/homework_6/task_1.py
@@ -5,9 +5,10 @@
 # Знаки препинания не должны оказаться внутри слова.
 # Если после слова идет запятая или точка, этот знак препинания должен идти
 # после того же слова, но уже преобразованного.
-
-some_text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
-             'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
+some_text = (
+    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
+    'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+)
 words = some_text.split()
 fin_words = []
 for word in words:
